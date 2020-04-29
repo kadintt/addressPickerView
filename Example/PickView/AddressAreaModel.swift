82172459aa12016e574f16e97ab78116f4e192b1
quchao <chao.qu@daoyitong.com>
2020-04-29 15:03:39 +0800

    提交代码

---
+++ Example/PickView/AddressAreaModel.swift
@@ -0,0 +1,13 @@
+//
+//  AddressAreaModel.swift
+//  PickView_Example
+//
+//  Created by 曲超 on 2020/4/29.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class AddressAreaModel: NSObject {
+
+}
